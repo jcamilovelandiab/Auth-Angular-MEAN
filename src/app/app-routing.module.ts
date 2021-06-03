b4061717c13b064797d20b144d203844a000fe82
Camilo <jcamilovelandiab@gmail.com>
2021-06-03 13:58:34 -0500

    Creating modules and components. Also, setting up routes and children routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule )
+    // loadChildren: './auth/auth.module.ts#AuthModule'
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./protected/protected.module').then( m => m.ProtectedModule )
+  },
+  {
+    path: '**',
+    redirectTo: 'auth'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
